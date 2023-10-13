33dc8182074de80763209faa961c2188aed391e3
fejkhfg <143350612+fejkhfg@users.noreply.github.com>
2023-10-13 16:29:06 -0500

    Update fetch.ts

--- src/backend/helpers/fetch.ts
+++ src/backend/helpers/fetch.ts
@@ -18,7 +18,7 @@ const baseFetch = ofetch.create({
   retry: 0,
 });
 
-export function baseRawFetch(url: string): {any} {
+export function baseRawFetch(url: string) {
   return baseFetch(url);
 }
 
