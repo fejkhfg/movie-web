89233d64538c7402f8d88cab199504f21978a79f
fejkhfg <143350612+fejkhfg@users.noreply.github.com>
2023-10-13 09:02:44 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://insane-crazy-movies-requests.fejkhfg.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzRkNDkyNzgyNGJkMjMwNDI1NTgzYTAyM2UxZWRhOCIsInN1YiI6IjY1MjZhYzY4ZWE4NGM3MDEwYzE5Y2RlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.51zh56tSxLp8OobYW027vMhXVUu752poq0asVkFLktw"
 };
