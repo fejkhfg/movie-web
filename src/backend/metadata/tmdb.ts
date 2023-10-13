db4e65f498aacf5f7ea74e2489b21100ab1b1948
fejkhfg <143350612+fejkhfg@users.noreply.github.com>
2023-10-13 17:25:17 -0500

    Update tmdb.ts

--- src/backend/metadata/tmdb.ts
+++ src/backend/metadata/tmdb.ts
@@ -173,7 +173,7 @@ function getImage<T>(url: string): Promise<T> {
 
 export function getMediaPoster(movieName: string | null, movieReleaseDate: number | null): string | undefined {
   if (movieReleaseDate && movieName) {
-    const promise = getImage<{'PromiseState': boolean, 'PromiseResult': {[key: string]: string}}>(`https://www.omdbapi.com/?apikey=daf26042&t=${movieName}&y=${movieReleaseDate}`);
+    const promise = getImage(`https://www.omdbapi.com/?apikey=daf26042&t=${movieName}&y=${movieReleaseDate}`);
 
     console.log(typeof(promise));
     console.log(promise);
@@ -183,7 +183,7 @@ export function getMediaPoster(movieName: string | null, movieReleaseDate: numbe
     }
   }
   if (movieName) {
-    const promise = getImage<{PromiseState: boolean, PromiseResult: {[key: string]: string}}>(`https://www.omdbapi.com/?apikey=daf26042&t=${movieName}`);
+    const promise = getImage(`https://www.omdbapi.com/?apikey=daf26042&t=${movieName}`);
 
     console.log(typeof(promise));
     console.log(promise);
