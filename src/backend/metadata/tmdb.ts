df54d3f913007303bf1ec4e1ea85169ce58ce883
fejkhfg <143350612+fejkhfg@users.noreply.github.com>
2023-10-13 13:12:57 -0500

    Update tmdb.ts

--- src/backend/metadata/tmdb.ts
+++ src/backend/metadata/tmdb.ts
@@ -163,7 +163,7 @@ export function getMediaDetails<
   throw new Error("Invalid media type");
 }
 
-function getImage(url: string): {Poster: string} {
+function getImage(url: string): Promise<Poster> {
   return baseRawFetch(url);
 }
 
